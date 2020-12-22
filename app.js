ab1fdeabaf13c96ddb6ed01c3a5ff6872f79b4f9
Raynnon <assante.f@gmail.com>
2020-12-22 23:40:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const utils = require("./utils.js");
+
+const sum = utils.add(4, -2);
+
+console.log(sum);
+console.log(utils.name);
---
+++ utils.js
@@ -0,0 +1,8 @@
+console.log("utils.js");
+
+module.exports = {
+  add: function (a, b) {
+    return a + b;
+  },
+  name: "Florian",
+};
